516d03be0f602c734761e2e37895da944652b3cd
Jake Waitze <jake@waitze.net>
2017-08-15 03:16:35 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import os, sys
+
+def GetFilesListFromDirectory(path):
+    file_list = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    return file_list
+
+image_path = 'C:\\Users\\user\\Desktop\\pps-work\\raw\\'
+done_path = 'C:\\Users\\user\\Desktop\\pps-work\\processed\\'
+if __name__ == '__main__':
+    files = GetFilesListFromDirectory(image_path)
+
+    max_number = 0
+    for file in files:
+        number = file[:file.index('.')]
+        if number.isdigit() and int(number) > max_number:
+            max_number = int(number)
+
+    for i in range(1, max_number, 2):
+        before = Image.open(image_path + str(i) + '.jpg')
+        after = Image.open(image_path + str(i+1) + '.jpg')
+        template = Image.open(image_path + 'template.jpg')
+
+        aspect_ratio_before = 340 / before.size[0]
+        aspect_ratio_after = 340 / after.size[0]
+
+        before_width = aspect_ratio_before * before.size[0]
+        before_height = aspect_ratio_before * before.size[1]
+        before.thumbnail((before_width, before_height))
+
+        after_width = aspect_ratio_after * after.size[0]
+        after_height = aspect_ratio_after * after.size[1]
+        after.thumbnail((after_width, after_height))
+
+        template_height = before_height if before_height > after_height else after_height
+        template_region = template.crop((0, 464 - (54 + template_height), 700, 464))
+        
+        template_region.paste(before, (0,0))
+        template_region.paste(after, (360,0))
+
+        template_region.save(done_path + str(((i-1)/2)+1) + '.jpg')
+        
+    print('Completed. %i images processed.' % max_number)        
